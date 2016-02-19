84abca2bcaf9dc2d50d93ae9681a0bd8476cfc99
EKemerov <eskemerov@gmail.com>
2016-02-19 10:18:01 +0700

    Настройки для postgres

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': '123qwerty123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
